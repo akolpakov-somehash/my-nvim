85415d07e4731d4c6e96a47f8a6d989dd8a75687
Oleksandr Kolpakov <akolpakov@tuble.pw>
2026-03-24 15:11:06 +0100

    LSP signature update

--- lua/plugins/signature.lua
+++ lua/plugins/signature.lua
@@ -1,6 +1,14 @@
 return {
 	"ray-x/lsp_signature.nvim",
 	event = "InsertEnter",
+	opts = {
+		hint_enable = false, -- disable inline virtual text if you only want the bubble
+		floating_window = true, -- show popup bubble
+		floating_window_above_cur_line = true,
+		handler_opts = {
+			border = "rounded",
+		},
+	},
 	keys = {
 		{
 			"<C-k>",
